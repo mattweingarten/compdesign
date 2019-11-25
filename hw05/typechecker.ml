2e0b8e9336b07f3fbddc3a007bbebe41bec3dce0
wmatt <matt@weingarten.org>
2019-11-25 20:52:28 +0100

    Adds statements and combines expression

--- hw05/typechecker.ml
+++ hw05/typechecker.ml
@@ -160,7 +160,121 @@ and typecheck_ret (l : 'a Ast.node) (tc : Tctxt.t) (t : Ast.ret_ty) : unit =
    a=1} is well typed.  (You should sort the fields to compare them.)
 
 *)
-let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
+let check_struct_subtypes (c : Tctxt.t) fs1 fs2 : bool =
+  let s1 = List.sort (fun (id1, _) (id2, _) -> compare id1 id2) fs1 in
+  let s2 =
+    List.sort
+      (fun { fieldName = id1 } { fieldName = id2 } -> compare id1 id2)
+      fs2
+  in
+  let subtys = List.map2 (fun (_, t1) { ftyp = t2 } -> subtype c t1 t2) s1 s2 in
+  List.fold_left ( && ) true subtys
+
+let check_subtypes c tys1 tys2 : bool =
+  List.fold_left ( && ) true
+    (List.map2 (fun ty1 ty2 -> subtype c ty1 ty2) tys1 tys2)
+
+let rec find_fields fs id =
+  match fs with
+  | { fieldName = name; ftyp = t } :: fs' ->
+      if id = name then t else find_fields fs' id
+  | [] -> raise Not_found
+
+  let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
+    match e.elt with
+    | CNull rty ->
+        typecheck_ty e c (TRef rty);
+        TNullRef rty
+    | CBool _ -> TBool
+    | CInt _ -> TInt
+    | CStr _ -> TRef RString
+    | Id id -> typecheck_exp_id e c id
+    | CArr (t, es) ->
+        typecheck_ty e c t;
+        if
+          List.fold_left ( && ) true
+            (List.map (fun exp -> subtype c (typecheck_exp c exp) t) es)
+        then t
+        else type_error e "illegal array declaration"
+    | NewArr (t, e1, x, e2) ->
+        typecheck_ty e c t;
+        if typecheck_exp c e1 != TInt then type_error e "array size not int";
+        if lookup_local_option x c != None then
+          type_error e @@ x ^ " already defined";
+        let e2_ty = typecheck_exp (add_local c x TInt) e2 in
+        if subtype c e2_ty t then e2_ty
+        else type_error e "invalid array definition"
+    | Index (e1, e2) ->
+        let arr_ty =
+          match typecheck_exp c e1 with
+          | TRef (RArray t) -> t
+          | _ -> type_error e "not array type"
+        in
+        if typecheck_exp c e2 = TInt then arr_ty else type_error e "invalid index"
+    | Length arr -> (
+        match typecheck_exp c arr with
+        | TRef (RArray t) -> TInt
+        | _ -> type_error e "not array, cannot get length" )
+    | CStruct (id, fs) ->
+        let struc =
+          match lookup_struct_option id c with
+          | None -> type_error e @@ "undefined struct " ^ id
+          | Some fs -> fs
+        in
+        if
+          check_struct_subtypes c
+            (List.map (fun (id, exp) -> (id, typecheck_exp c exp)) fs)
+            struc
+          = false
+        then type_error e "invalid struct fields";
+        TRef (RStruct id)
+    | Proj (exp, id) -> (
+        let s_id =
+          match typecheck_exp c exp with
+          | TRef (RStruct id) -> id
+          | _ -> type_error e "not a struct"
+        in
+        let s_fs =
+          match lookup_struct_option id c with
+          | Some fs -> fs
+          | _ -> type_error e "undefined struct"
+        in
+        try find_fields s_fs id
+        with Not_found ->
+          type_error e @@ "field " ^ id ^ " not defined for struct" )
+    | Call (f, args) ->
+        let f_args, f_ret =
+          match typecheck_exp c f with
+          | TRef (RFun (args, RetVal retty)) -> (args, retty)
+          | _ -> type_error e "not function"
+        in
+        let args_tys = List.map (fun e -> typecheck_exp c e) args in
+        if check_subtypes c args_tys f_args then f_ret
+        else type_error e "invalid argument types"
+    | Bop (Eq, e1, e2) | Bop (Neq, e1, e2) ->
+        let e1_ty = typecheck_exp c e1 in
+        let e2_ty = typecheck_exp c e2 in
+        if subtype c e1_ty e2_ty && subtype c e2_ty e1_ty then TBool
+        else type_error e "invalid comparison types"
+    | Bop (bop, e1, e2) ->
+        let e1_ty = typecheck_exp c e1 in
+        let e2_ty = typecheck_exp c e2 in
+        let t1, t2, final = typ_of_binop bop in
+        if e1_ty = t1 && e2_ty = t2 then final
+        else type_error e "invalid bop types"
+    | Uop (uop, exp) ->
+        let u_ty = typ_of_unop uop in
+        let e_ty = typecheck_exp c exp in
+        if e_ty = fst u_ty then snd u_ty else type_error e "illegal uop type"
+
+  and typecheck_exp_id (e : Ast.exp node) (c : Tctxt.t) (id : Ast.id) : Ast.ty =
+    match Tctxt.lookup_local_option id c with
+    | None -> lookup_global id c
+    | Some t -> t
+
+
+
+(* let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
   let error = TypeError("Type mismatch in expression: " ^ (string_of_exp e) ^
                         " at location" ^ (Range.string_of_range e.loc)) in
 
@@ -193,7 +307,7 @@ let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
     | Bop (op, e1, e2) ->typecheck_binop op e1 e2
     | Uop (op, e) -> typecheck_unop op e
     (* | _ -> failwith "unimplemented" *)
-  end
+  end *)
 
 (* statements --------------------------------------------------------------- *)
 
@@ -230,6 +344,16 @@ let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
 *)
 let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.t * bool =
 
+  let rec typecheck_block (tc: Tctxt.t) (block:Ast.block) (to_ret:ret_ty) :Tctxt.t * bool =
+    begin match block with
+      | h::[] -> typecheck_stmt tc h to_ret
+      | h::t -> let c,b = typecheck_stmt tc h to_ret in if b
+                then type_error h ("Invalid return") else typecheck_block c t to_ret
+      | [] -> type_error (Ast.no_loc "") ("empty block")
+  end
+  in
+
+
   let type_ass (lhs:exp node) (rhs:exp node):  Tctxt.t * bool =
     let t = typecheck_exp tc lhs in
     let t' = typecheck_exp tc rhs in
@@ -260,7 +384,13 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
     ((add_local tc id t),false)
   in
 
-  let type_scall (fname:exp node) (args: exp node list) :Tctxt.t * bool=
+  let type_decls (vdecls :vdecl list) :Tctxt.t * bool =
+    let new_c = List.fold_left(fun c (id,exp) -> fst @@ typecheck_stmt c {elt=(Decl(id, exp));loc=s.loc} to_ret) tc vdecls
+    in (new_c, false)
+  in
+
+
+  let type_scall (fname:exp node) (args: exp node list) :Tctxt.t * bool =
     let id = match fname.elt with | Id id -> id | _ -> type_error s ("Invalid function name") in
     let args_t' = List.map (fun e -> typecheck_exp tc e) args in
     let args_t,return = match typecheck_exp tc fname with
@@ -272,21 +402,72 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
   in
 
 
+  let type_if (cond: exp node) (ifb:block) (elseb:block) :Tctxt.t * bool =
+      let c = typecheck_exp tc cond in
+      if (c != Ast.TBool) then type_error s ("non boolean condition in if statement");
+      let ci,rti = typecheck_block tc ifb to_ret in
+      let ce, rte = typecheck_block ci elseb to_ret in
+      (tc,(rti && rte))
+  in
+
+
+  let type_ret (exp:exp node option) :Tctxt.t * bool =
+
+
+    begin match to_ret with
+      | RetVoid -> begin match exp with | None -> (tc,true) | Some _  -> type_error s ("Function expects return void") end
+      | RetVal ret_t ->
+        begin match exp with
+          | None -> type_error s ("Function returns void but expects return of type: " ^ (string_of_ty ret_t))
+          | Some e ->
+            let t_exp = typecheck_exp tc e in
+            let ret_check = subtype tc t_exp ret_t in
+            if (ret_check = false) then type_error s ("return type " ^ (string_of_ty t_exp) ^ " is not subset of " ^ (string_of_ty ret_t));
+            (tc,true)
+      end
+    end
+
+
+  in
+
+
+  let type_for (vdecls: vdecl list) (eo: exp node option) (so: stmt node option) (body:block) :Tctxt.t * bool =
+    let tc1 = fst @@ type_decls vdecls in
+    let exp_t = begin match eo with
+      | Some exp -> typecheck_exp tc1 exp
+      | None -> TBool
+    end in
+    if(exp_t != TBool) then type_error s ("Condition expression has type: " ^ (string_of_ty exp_t) ^ " instead of boolean in for loop");
+    let stmt_ret = begin match so with
+      | Some stmt -> snd @@ typecheck_stmt tc1 stmt to_ret
+      | None -> false
+    end in
+    if(stmt_ret = true) then type_error s ("For loop statement returns, which is not allowed!");
+    let a,b = typecheck_block tc1 body to_ret in
+    (tc, false)
+  in
+
+  let type_while (cond :exp node) (body: block) :Tctxt.t * bool =
+    let exp_t = typecheck_exp tc cond in
+    if(exp_t != TBool) then type_error s ("Condition expression has type: " ^ (string_of_ty exp_t) ^ " instead of boolean in while loop");
+    let a,b = typecheck_block tc body to_ret in
+    (tc,false)
+  in
+
+
   begin match s.elt with
     | Assn (lhs,rhs) -> type_ass lhs rhs
     | Decl  (id, e) -> type_decl id e
-    | Ret exp -> failwith "unimplemented"
+    | Ret exp -> type_ret exp
     | SCall (fname,args) -> type_scall fname args
-    | If (cond, ifb, elseb) -> failwith "unimplemented"
-    | Cast _ -> failwith "unimplemented"
-    | For (vdecls, eo, so, body) -> failwith "unimplemented"
-    | While (cond, body) -> failwith "unimplemented"
+    | If (cond, ifb, elseb) -> type_if cond ifb elseb
+    | Cast _ -> failwith "cast unimplemented"
+    | For (vdecls, eo, so, body) -> type_for vdecls eo so body
+    | While (cond, body) -> type_while cond body
   end
 
 
-
-
-let rec typecheck_block (tc: Tctxt.t) (block:Ast.block) (to_ret:ret_ty) :Tctxt.t * bool =
+ let rec typecheck_block (tc: Tctxt.t) (block:Ast.block) (to_ret:ret_ty) :Tctxt.t * bool =
   begin match block with
     | h::[] -> typecheck_stmt tc h to_ret
     | h::t -> let c,b = typecheck_stmt tc h to_ret in if b
